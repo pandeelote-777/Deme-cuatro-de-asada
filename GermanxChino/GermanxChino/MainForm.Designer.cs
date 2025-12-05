da0d5312cff035f06a633ba5d225c4f2f84e4bb8
Axelzei <viaa091201hgtylxa9@cbtis217.edu.mx>
2025-12-04 18:48:46 -0600

    Se empezo a hacer el login

--- GermanxChino/GermanxChino/MainForm.Designer.cs
+++ GermanxChino/GermanxChino/MainForm.Designer.cs
@@ -36,12 +36,98 @@ namespace GermanxChino
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtUsiario = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtContraseña = new System.Windows.Forms.TextBox();
+			this.btnEntrar = new System.Windows.Forms.Button();
+			this.btnSalir = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(66, 7);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(160, 37);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Login";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(26, 54);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(66, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Usuario:";
+			// 
+			// txtUsiario
+			// 
+			this.txtUsiario.Location = new System.Drawing.Point(111, 54);
+			this.txtUsiario.Name = "txtUsiario";
+			this.txtUsiario.Size = new System.Drawing.Size(151, 22);
+			this.txtUsiario.TabIndex = 2;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(26, 81);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(82, 23);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Contraseña:";
+			// 
+			// txtContraseña
+			// 
+			this.txtContraseña.Location = new System.Drawing.Point(111, 83);
+			this.txtContraseña.Name = "txtContraseña";
+			this.txtContraseña.Size = new System.Drawing.Size(151, 22);
+			this.txtContraseña.TabIndex = 4;
+			// 
+			// btnEntrar
+			// 
+			this.btnEntrar.Location = new System.Drawing.Point(182, 122);
+			this.btnEntrar.Name = "btnEntrar";
+			this.btnEntrar.Size = new System.Drawing.Size(103, 23);
+			this.btnEntrar.TabIndex = 5;
+			this.btnEntrar.Text = "Entrar";
+			this.btnEntrar.UseVisualStyleBackColor = true;
+			// 
+			// btnSalir
+			// 
+			this.btnSalir.Location = new System.Drawing.Point(26, 122);
+			this.btnSalir.Name = "btnSalir";
+			this.btnSalir.Size = new System.Drawing.Size(97, 23);
+			this.btnSalir.TabIndex = 6;
+			this.btnSalir.Text = "Salir";
+			this.btnSalir.UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "GermanxChino";
+			this.ClientSize = new System.Drawing.Size(297, 175);
+			this.Controls.Add(this.btnSalir);
+			this.Controls.Add(this.btnEntrar);
+			this.Controls.Add(this.txtContraseña);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtUsiario);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Name = "MainForm";
+			this.Text = "GermanxChino";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Button btnSalir;
+		private System.Windows.Forms.Button btnEntrar;
+		private System.Windows.Forms.TextBox txtContraseña;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtUsiario;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
 	}
 }
