91c1fd1a8ec91a5e9507aa2e5e6c0cab444cb3b4
pandeelote-777 <andrearuizmagana@gmail.com>
2025-12-04 18:34:05 -0600

    Crear archivo de SharpDevelop GermanxChino jaja salu2

---
+++ GermanxChino/GermanxChino/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: andre
+ * Fecha: 04/12/2025
+ * Hora: 06:32 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GermanxChino
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
