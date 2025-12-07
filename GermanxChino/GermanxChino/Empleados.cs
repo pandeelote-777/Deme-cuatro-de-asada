07530f891e8f6429f7c22309e93fe6bdb6a4cd45
Axelzei <viaa091201hgtylxa9@cbtis217.edu.mx>
2025-12-06 21:25:49 -0600

    Se creo el menú con las cosas que llevamos y el formulario enpleados

--- GermanxChino/GermanxChino/Empleados.cs
+++ GermanxChino/GermanxChino/Empleados.cs
@@ -1,24 +1,23 @@
 ﻿/*
  * Creado por SharpDevelop.
- * Usuario: andre
- * Fecha: 04/12/2025
- * Hora: 06:32 p. m.
+ * Usuario: tijam
+ * Fecha: 06/12/2025
+ * Hora: 09:07 p. m.
  * 
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
 namespace GermanxChino
 {
 	/// <summary>
-	/// Description of MainForm.
+	/// Description of Empleados.
 	/// </summary>
-	public partial class MainForm : Form
+	public partial class Empleados : Form
 	{
-		public MainForm()
+		public Empleados()
 		{
 			//
 			// The InitializeComponent() call is required for Windows Forms designer support.
@@ -28,20 +27,6 @@ namespace GermanxChino
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
-		}
-		
-		void BtnSalirClick(object sender, EventArgs e)
-		{
-			Application.Exit();
-		}
-		
-		void BtnEntrarClick(object sender, EventArgs e)
-		{
-			if(txtUsiario.Text=="Admin" && txtContraseña.Text=="123"){
-				Menu f =new Menu();
-				f.Show();
-				this.Hide();
-			}
 		}
 	}
 }
---
+++ GermanxChino/GermanxChino/Menu.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: tijam
+ * Fecha: 06/12/2025
+ * Hora: 07:39 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GermanxChino
+{
+	/// <summary>
+	/// Description of Menu.
+	/// </summary>
+	public partial class Menu : Form
+	{
+		Postres_Pasteleria_ p = new Postres_Pasteleria_();
+		Empleados Em = new Empleados();
+		public Menu()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}	
+		void TolPasteleriaClick(object sender, EventArgs e)
+		{
+			p.MdiParent=this;
+			p.Show();
+			this.Em.Hide();
+		}
+		
+		void TolEmpleadosClick(object sender, EventArgs e)
+		{
+			Em.MdiParent=this;
+			Em.Show();
+			this.p.Hide();
+		}
+		
+		void TolSalirClick(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
