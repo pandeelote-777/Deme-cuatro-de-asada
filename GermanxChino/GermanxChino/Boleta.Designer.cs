27326208d4701a71ea28c4deb5e3f11b6e338e59
danii-tc <tocw090913mgtrhna0@cbtis217.edu.mx>
2025-12-12 14:02:57 -0600

    Se creo la boleta de calificaciones

---
+++ GermanxChino/GermanxChino/Boleta.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: a4dew
+ * Fecha: 12/12/2025
+ * Hora: 01:51 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace GermanxChino
+{
+	partial class Boleta
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// Boleta
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "Boleta";
+			this.Name = "Boleta";
+		}
+	}
+}
---
+++ GermanxChino/GermanxChino/Boleta.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: a4dew
+ * Fecha: 12/12/2025
+ * Hora: 01:51 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GermanxChino
+{
+	/// <summary>
+	/// Description of Boleta.
+	/// </summary>
+	public partial class Boleta : Form
+	{
+		public Boleta()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
